41448442b76d1dbb0113d03bfb466432f8e4db21
Alex <94067525+Riku70X@users.noreply.github.com>
2023-03-14 10:11:00 +0000

    Created BoundingSphere
    Untested

---
+++ BegginerVectors/Assets/Scripts/BoundingObjects/MyBoundingSphere.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MyBoundingSphere
+{
+    MyVector3 centrepoint;
+    float radius;
+
+    MyBoundingSphere(MyTransformComponent transform, float radius)
+    {
+        centrepoint = transform.position;
+        this.radius = radius;
+    }
+
+    public bool isOverlappingWith(MyBoundingSphere otherSphere)
+    {
+        float radiusSumDistanceSq = radius + otherSphere.radius;
+        radiusSumDistanceSq *= radiusSumDistanceSq;
+        float centreDistanceSq = (centrepoint - otherSphere.centrepoint).GetVectorLengthSquared();
+        return centreDistanceSq < radiusSumDistanceSq;
+    }
+}
